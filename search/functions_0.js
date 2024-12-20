b9dd7a22fc5abc4a9844efc7c0bcd661b8f714af
alicebeth-m <alicebeth-m@users.noreply.github.com>
2024-12-20 18:51:18 +0000

    deploy: 8bb316041f4c261cc317a3f796059c865cae5b1d

---
+++ search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['factorial_0',['factorial',['../mathlib_8h.html#ae1b37c26bb8e5744f5747d6cd6505356',1,'mathlib.h']]]
+];
---
+++ search/functions_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['is_5fprime_0',['is_prime',['../mathlib_8h.html#a4583e2d9b8c108304fbeda300d2a4450',1,'mathlib.h']]]
+];
